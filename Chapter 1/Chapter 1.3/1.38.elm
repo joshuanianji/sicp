6bd611cb08cf78a59bfedd6a4f888484e23e4259
Joshua Ji <joshua.ji@jiwhiz.com>
2018-08-14 10:37:58 -0600

    added some more comments

--- Chapter 1/Chapter 1.3/1.38.elm
+++ Chapter 1/Chapter 1.3/1.38.elm
@@ -2,35 +2,40 @@ module Main exposing (main)
 
 import Html exposing (Html)
 
+
 return1 x =
     1
 
+
 returnDaddyEuler n =
     if rem (n + 1) 3 == 0 then
-        (toFloat (n + 1)) * 2 / 3
+        toFloat (n + 1) * 2 / 3
     else
         1
 
+
 contFracRecursive : (Float -> Float) -> (Float -> Float) -> Int -> Float -> Float
-contFracRecursive n d k i = 
+contFracRecursive n d k i =
     if k == 1 then
-        (n i) / (d i)
-    else 
-        (n i) / ((d i) + (contFracRecursive n d (k - 1) (i + 1)))
-    
+        n i / d i
+    else
+        n i / (d i + contFracRecursive n d (k - 1) (i + 1))
+
+
 contFrac n d k =
     contFracRecursive n d k 1
-    
+
+
 contFracIter n d k answer =
     if k == 0 then
         answer
     else
-        contFracIter n d (k - 1) ( (n k) / ( (d k) + answer) )
-    
+        contFracIter n d (k - 1) (n k / (d k + answer))
+
+
 main : Html msg
 main =
     contFracIter return1 returnDaddyEuler 100000 0
         |> (+) 2
         |> toString
         |> Html.text
-
