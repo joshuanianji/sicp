8ebb94a769d61f2bea820812a06dcc5fc0c83c6d
Joshua Ji <joshua.ji@jiwhiz.com>
2018-08-20 14:33:09 -0600

    finished 2.58.2 (kinda - it's incorrect though)

---
+++ Chapter 2/Chapter 2.3/2.58.2-incorrect.scm
@@ -0,0 +1,112 @@
+;; derivatives in infix notation but does not do order of operations corrently - can only do from left to right.
+
+(define (deriv exp var)
+    (cond 
+        ((number? exp) 0)
+
+        ((variable? exp)
+            (if (same-variable? exp var) 1 0))
+
+        ((sum? exp)
+            (make-sum
+                (deriv (addend exp) var)
+                (deriv (augend exp) var)))
+
+        ((product? exp)
+            (make-sum
+                (make-product 
+                    (multiplier exp)
+                    (deriv (multiplicand exp) var))
+            (make-product 
+                (deriv (multiplier exp) var)
+                (multiplicand exp))))
+
+        ((exponentiation? exp)
+            (make-product
+                (make-product
+                    (exponent exp)
+                    (make-exponentiation 
+                        (base exp)
+                        (make-sum
+                            (exponent exp)
+                            (- 1))))
+                (deriv (base exp) var)))
+          
+        (else (error "unknown expression 
+                      type: DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+    (and 
+        (variable? v1)
+        (variable? v2)
+        (eq? v1 v2)))
+
+;; sum
+(define (sum? x)
+    (and (pair? x) (eq? (cadr x) '+)))
+
+(define (addend s) (car s))
+
+
+(define (augend expr)
+    ; if length is greater than 3
+    (if (> (length expr) 3)
+        (cddr expr)
+        (caddr expr)
+    ))
+
+
+;; product
+(define (product? x)
+    (and (pair? x) (eq? (cadr x) '*)))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand expr)
+    ; if length is greater than 3
+    (if (> (length expr) 3)
+        (cddr expr)
+        (caddr expr)
+    ))
+
+;; exponentiation
+
+(define (exponentiation? x)
+    (and (pair? x) (eq? (cadr x) '**)))
+
+(define (base s) (car s))
+
+(define (exponent s) (cddr s))
+
+;; creating the operations
+
+(define (make-sum a1 a2)
+    (cond   
+        ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) 
+            (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (=number? exp num)
+    (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+    (cond   
+        ((or (=number? m1 0) (=number? m2 0)) 
+            0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2))
+            (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (make-exponentiation base exponent)
+    (cond   
+        ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        ((and (number? base) (number? exponent)) 
+            (expt base exponent))
+        (else (list base '** exponent))))
